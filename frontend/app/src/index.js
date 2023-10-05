942eff8194fbc90b5a067a83fcb412f1571bec3c
hikoyara <hikoyara7@icloud.com>
2023-10-05 17:08:56 +0900

    install firebase

--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -7,6 +7,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./routes/SignIn";
 import SignUp from "./routes/SignUp";
 import Board from "./routes/Board";
+/* firebase */
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDR6FWcPKZn3J0WiLqyXKDISMR2CDV_rvs",
+    authDomain: "boardapp-56c15.firebaseapp.com",
+    projectId: "boardapp-56c15",
+    storageBucket: "boardapp-56c15.appspot.com",
+    messagingSenderId: "696519991355",
+    appId: "1:696519991355:web:a87a2f7d4e59496b2fbc1e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -21,7 +36,4 @@ root.render(
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
