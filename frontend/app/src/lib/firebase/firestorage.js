c017cc7fdf812964e228b54a047d4203df877423
LongTown <longtown@LongTownnoMac-mini-2.local>
2023-10-18 14:15:41 +0900

    front,back Members

--- frontend/app/src/lib/firebase/firestorage.js
+++ frontend/app/src/lib/firebase/firestorage.js
@@ -1,17 +1,41 @@
 import { db } from "../config";
 /* firebase */
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, Timestamp, onSnapshot, query, orderBy } from "firebase/firestore";
 
 export const createMember = async (id, password) => {
     try {
         const member = await addDoc(collection(db, "members"), {
             id,
             password,
+            state: "Unauthenticated",
+            updatedAt: Timestamp.fromDate(new Date()),
         });
         console.log("組合員登録成功");
         return member;
     } catch (error) {
         console.error("組合員登録エラー", error);
+        throw error;
+    }
+};
+
+export const getMembers = async (callback) => {
+    try {
+        const ref = collection(db, "members");
+        const q = query(ref, orderBy("updatedAt", "desc"));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            const membersData = [];
+            snapshot.forEach((doc) => {
+                membersData.push({
+                    id: doc.data().id,
+                    state: doc.data().state,
+                    updatedAt: doc.data().updatedAt,
+                });
+            });
+            callback(membersData);
+        });
+        return unsubscribe;
+    } catch (error) {
+        console.error("組合員取得エラー", error);
         throw error;
     }
 };
--- frontend/app/src/routes/Member/Members.jsx
+++ frontend/app/src/routes/Member/Members.jsx
@@ -1,19 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 /* components */
 import Dashboard from "../../components/Dashboard";
+import CustomizedTables from "../../components/Table";
 /* lib */
 import { getUser } from "../../lib/firebase/auth";
+import { getMembers } from "../../lib/firebase/firestorage";
 /* router */
 import { useNavigate } from "react-router-dom";
 
 const Members = () => {
     const navigate = useNavigate();
 
+    const [members, setMembers] = useState([]);
+
     useEffect(() => {
         const register = async () => {
             const user = await getUser();
             if (!user) {
                 navigate("/");
+            } else {
+                const unsubscribe = await getMembers((membersData) => {
+                    setMembers(membersData);
+                });
+                return () => unsubscribe();
             }
         };
 
@@ -22,7 +31,7 @@ const Members = () => {
 
     return (
         <Dashboard selected={3}>
-            <p>メンバー</p>
+            <CustomizedTables members={members} />
         </Dashboard>
     );
 };
